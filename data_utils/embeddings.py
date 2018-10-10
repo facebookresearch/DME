a3217eedb69ceb15a472f6ccebc048b4a1cdc005
Changhan Wang <changhan@fb.com>
2018-10-10 09:18:30 -0700

    updated copyright headers

--- data_utils/embeddings.py
+++ data_utils/embeddings.py
@@ -1,3 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 embeddings = [
         ('fasttext', 'crawl-300d-2M.vec', 300, 'fastText CommonCrawl',
          'https://s3-us-west-1.amazonaws.com/fasttext-vectors/crawl-300d-2M.vec.zip'),
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,3 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 from ImageCaptionRetrievalTask import ImageCaptionRetrievalTask
 from LanguageInferenceTask import LanguageInferenceTask
 from TextClassificationTask import TextClassificationTask
